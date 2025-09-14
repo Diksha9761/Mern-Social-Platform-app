f8b4d34b198284fbf8f161019b94fc8685062668
Diksha9761 <deekshabisht9761@gmail.com>
2025-09-14 14:07:35 +0530

    Update SocketContext.jsx

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -5,7 +5,7 @@ import { createContext } from "react";
 import {io} from 'socket.io-client';
 import { UserData } from "./UserContext";
 
-const EndPoint = "http://localhost:7000";
+const EndPoint = "https://mern-social-platform-app.onrender.com";
 
 const SocketContext = createContext();
 
